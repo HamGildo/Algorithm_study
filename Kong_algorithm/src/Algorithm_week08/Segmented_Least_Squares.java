1763e8e25a084a0c506d934d13bb7f24e8a3ad12
HamGildo <ham5477@naver.com>
2019-11-06 19:51:23 +0900

    Modify make_SLS method, NaN error handling

--- Kong_algorithm/src/Algorithm_week08/Segmented_Least_Squares.java
+++ Kong_algorithm/src/Algorithm_week08/Segmented_Least_Squares.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Segmented_Least_Squares {
-    public double make_SLS(int n, Point[] p, int c) {
+    public double make_SLS(int n, Point[] p, double c) {
         double[] OPT = new double[n+1];
         OPT[0] = 0;
         double[][] errorSum = new double[n+1][n+1]; // i ~ j 까지의 SSE를 저장하는 배열
@@ -20,28 +20,45 @@ public class Segmented_Least_Squares {
         for(int j = 1; j < n+1; j++) {
             for(int i = 1; i < j+1; i++) {
                 for(int k = i; k < j+1; k++) {
-                    Sxy += p[k-1].getX() * p[k].getY();
+                    Sxy += p[k-1].getX() * p[k-1].getY();
                     Sx += p[k-1].getX();
                     Sy += p[k-1].getY();
-                    Sx_2 += Math.pow(p[k].getX(), 2);
+                    Sx_2 += p[k-1].getX()*p[k-1].getX();
                 }
-                N = j - i + 1;
-                a = (N*Sxy - Sx*Sy) / (N*Sx_2 - Math.pow(Sx,2));
-                b = (Sy - a*Sx)/N;
 
-                a_arr[i][j] = a;
-                b_arr[i][j] = b;
+                if(i != j) {
+                    N = j - i + 1;
+                    a = (N * Sxy - Sx * Sy) / (N * Sx_2 - Sx * Sx);
+                    b = (Sy - a * Sx) / N;
+                    a_arr[i][j] = a;
+                    b_arr[i][j] = b;
+                }
+               else {
+                    a_arr[i][j] = 0;
+                    b_arr[i][j] = 0;
+                }
 
                 for(int k2 = i; k2 < j+1; k2++) {
-                    errorSum[i][j] += Math.pow((p[k2-1].getY() - a*p[k2-1].getX() - b),2);
+                    errorSum[i][j] += Math.pow((p[k2-1].getY() - a_arr[i][j]*p[k2-1].getX() - b_arr[i][j]),2);
                 }
+                Sxy=0;
+                Sx=0;
+                Sy=0;
+                Sx_2=0;
             }
         }
         //OPT배열 생성
+        double min = Double.POSITIVE_INFINITY;
+        double temp;
         for(int j = 1; j < n+1; j++) {
             for(int i = 1; i < j+1; i++) {
-               OPT[j] = errorSum[i][j] + c + OPT[i-1];
+                temp =  errorSum[i][j] + c + OPT[i-1];
+                if(temp < min) {
+                    min = temp;
+                }
             }
+            OPT[j] = min;
+            min = Double.POSITIVE_INFINITY;
         }
         //최종값 리턴
         return OPT[n];
@@ -56,11 +73,12 @@ public class Segmented_Least_Squares {
         //바이트 -> 문자열 -> 파싱
         String input = new String(readBuffer);
         String[] inputs = input.split(",");
-        //int배열 생성
+        //double배열 생성
         double[] arrayA = new double[inputs.length];
         for(int i = 0; i < arrayA.length; i++) {
             arrayA[i] = Double.parseDouble(inputs[i]);
         }
+        //계산에 필요한 변수들에 값을 넣어준다.
         int n = (int)arrayA[0];
         double c =  arrayA[arrayA.length-1];
         //point 배열을 생성한다.
@@ -68,6 +86,9 @@ public class Segmented_Least_Squares {
         for(int i = 0; i < n; i++){
             points[i] = new Point(arrayA[2*i+1],arrayA[2*i+2]);
         }
+
+        Segmented_Least_Squares test = new Segmented_Least_Squares();
+        System.out.println("OPT cost: "+ test.make_SLS(n,points,c));
     }
 
 
