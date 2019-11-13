e69907aa335cd2218497f7b8f0bc8d6f4366f06f
HamGildo <ham5477@naver.com>
2019-11-14 03:32:43 +0900

    Make Constructor and optArray method

--- Kong_algorithm/src/Algorithm_week10/Knapsack.java
+++ Kong_algorithm/src/Algorithm_week10/Knapsack.java
@@ -1,4 +1,39 @@
 package Algorithm_week10;
 
 public class Knapsack {
+    private int[][] OPT;
+
+    public Knapsack(int n, int w) {
+        OPT = new int[n+1][w+1];
+        for(int i = 0; i < w+1; i++) {
+            OPT[0][i] = 0;
+        }
+    }
+
+    public int[][] optArray(Item[] items, int weight){
+        for(int i = 1; i < items.length+1; i++) {
+            for(int w = 1; w < weight+1; w++) {
+                int temp1 = OPT[i-1][w];
+                int temp2 = items[i].getValue() + OPT[i-1][w-(items[i].getWeight())];
+                if (items[i].getWeight() > w) {
+                   OPT[i][w] = OPT[i-1][w];
+                }
+                else if(temp1 > temp2){
+                    OPT[i][w] = temp1;
+                }
+                else {
+                    OPT[i][w] = temp2;
+                }
+            }
+        }
+
+        return OPT;
+    }
+
+
+
+
+
+
+
 }
