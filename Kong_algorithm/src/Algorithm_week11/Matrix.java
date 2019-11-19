f6afb81da617cca9c73e4e29063984eb83afe479
HamGildo <ham5477@naver.com>
2019-11-19 18:11:18 +0900

    Add Matrix class

---
+++ Kong_algorithm/src/Algorithm_week11/Matrix.java
@@ -0,0 +1,19 @@
+package Algorithm_week11;
+
+public class Matrix {
+    private int row;
+    private int column;
+
+    public Matrix(int r, int c) {
+        row = r;
+        column = c;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
---
+++ Kong_algorithm/src/Algorithm_week11/Matrix_chain_multiplication.java
@@ -0,0 +1,5 @@
+package Algorithm_week11;
+
+public class Matrix_chain_multiplication {
+    public
+}
