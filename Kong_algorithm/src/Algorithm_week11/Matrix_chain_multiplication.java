9716ebff18c3046e5157fc14cb6ddbd6bd66efff
HamGildo <ham5477@naver.com>
2019-11-19 19:14:02 +0900

    Add Matrix_chain-order method

--- Kong_algorithm/src/Algorithm_week11/Matrix_chain_multiplication.java
+++ Kong_algorithm/src/Algorithm_week11/Matrix_chain_multiplication.java
@@ -1,5 +1,29 @@
 package Algorithm_week11;
 
 public class Matrix_chain_multiplication {
-    public
+    public int[][] Matrix_chain_order(Matrix[] matrices) {
+        int n = matrices.length;
+        int[][] m = new int[n][n];
+
+        for(int i = 1; i < n; i++){
+            m[i][i] = 0;
+        }
+        for(int l = 2; l < n; l++) { // l = chain_length
+            for(int i = 1; i < n-l+1; i++) {
+                int j = i + l - 1;
+                m[i][j] = Integer.MAX_VALUE;
+                for(int k = i; k < j; k++) {
+                    int q = m[i][k] + m[k+1][j]
+                            + matrices[i].getRow()*matrices[k].getColumn()*matrices[j].getColumn();
+                    if(q < m[i][j]) {
+                        m[i][j] = q;
+                    }
+                }
+            }
+        }
+
+        return m;
+    }
+
+
 }
