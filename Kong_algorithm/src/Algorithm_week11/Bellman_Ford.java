880e1ac54d3b35145339e74da69c43b8ba300fba
HamGildo <ham5477@naver.com>
2019-11-19 21:19:07 +0900

    Remove Graph class

--- Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
+++ Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
@@ -1,6 +1,19 @@
 package Algorithm_week11;
 
+import java.util.ArrayList;
+
 public class Bellman_Ford {
+
+    public boolean bellman_ford() {
+
+        return true;
+    }
+
+    private void init_single_source() {
+
+
+    }
+
 
 
 }
