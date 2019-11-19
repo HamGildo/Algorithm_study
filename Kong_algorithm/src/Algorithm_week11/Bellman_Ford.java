886a7e868a80b1dc36d2de317364f940da54c3ee
HamGildo <ham5477@naver.com>
2019-11-19 21:34:05 +0900

    Add init_single_source method

--- Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
+++ Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 
 public class Bellman_Ford {
 
-    public boolean bellman_ford() {
+    public boolean bellman_ford(int[] graph, int[][] w, int s) {
+        init_single_source(graph, s);
 
         return true;
     }
 
-    private void init_single_source() {
-
-
+    private void init_single_source(int[] graph, int s) { // 시작정점을 제외하고 무한대로 설정
+        for(int i = 0; i < graph.length; i++) {
+            if(i == s) continue;
+            graph[i] = (int)Double.POSITIVE_INFINITY;
+        }
     }
 
 
