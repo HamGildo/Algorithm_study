d8330ffc6418995b7a061ddc37e36a37265fea09
HamGildo <ham5477@naver.com>
2019-11-19 21:48:09 +0900

    Add Edge class

--- Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
+++ Kong_algorithm/src/Algorithm_week11/Bellman_Ford.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class Bellman_Ford {
 
-    public boolean bellman_ford(int[] graph, int[][] w, int s) {
+    public boolean bellman_ford(double[] graph, int[][] w, int s) {
         init_single_source(graph, s);
+        for(int i = 0; i < graph.length-1; i++) { // 정점의 수 -1 만큼 반복
+
+        }
 
         return true;
     }
 
-    private void init_single_source(int[] graph, int s) { // 시작정점을 제외하고 무한대로 설정
+    private void init_single_source(double[] graph, int s) { // 시작정점을 제외하고 무한대로 설정
         for(int i = 0; i < graph.length; i++) {
-            if(i == s) continue;
-            graph[i] = (int)Double.POSITIVE_INFINITY;
+            if(i == s) graph[i] = 0;
+            else graph[i] = Double.POSITIVE_INFINITY;
         }
     }
 
